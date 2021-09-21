49e7b72276cb0a4b369d2165fb6670a07645b047
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-21 17:05:29 +0100

    first commit

---
+++ python_exercises/guess_number.py
@@ -0,0 +1,12 @@
+guess = int(input("Enter a number\n"))
+
+
+if guess > 100:
+    print("Guess is too High")
+
+if guess < 100:
+    print("Guess is too low")
+
+if guess == 100:
+    print("Absolutely Correct")
+    
